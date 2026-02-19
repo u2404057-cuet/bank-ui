85becede56a7e25d988df1d1fbac83e8970ff6d2
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-02-19 12:18:21 +0600

    login btn working now

--- script/login.js
+++ script/login.js
@@ -1,1 +1,15 @@
-console.log('login successful');
+console.log("login successful");
+document.getElementById("login-btn").addEventListener("click", function () {
+  const inputNumber = document.getElementById("input-number");
+  const number = inputNumber.value;
+  const inputPassword = document.getElementById("input-password");
+  const password = inputPassword.value;
+  console.log(number, password);
+
+  if (number == "01797678762" && password == "1234") {
+    window.location.assign("./home.html");
+  } else {
+    alert("login failed");
+    return;
+  }
+});
